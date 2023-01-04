6efc41c991e5989676789e621c39ee7b1c7a3e0c
SkeppysFlipFlops <57578841+SkeppysFlipFlops@users.noreply.github.com>
2023-01-04 02:32:25 -0500

    version bump

--- Source/DebugMod.cs
+++ Source/DebugMod.cs
@@ -176,7 +176,7 @@ namespace DebugMod
         
         public override string GetVersion()
         {
-            string version = "1.5.6";
+            string version = "1.5.7";
 #if DEBUG
             version = string.Concat(version, "-dev");
 #endif
